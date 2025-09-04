aa9c1e6165b5ef0097aaf1b7618b6b263e98e8c7
Minaltawale <minaltawale03@gmail.com>
2025-09-04 20:09:53 +0530

    added HTML files with CSS

--- expenseTracker/settings.py
+++ expenseTracker/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'expenseTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ExpenseTracker',
+        'USER': 'postgres',
+        'PASSWORD': 'Minal@123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -115,7 +119,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "public/static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
